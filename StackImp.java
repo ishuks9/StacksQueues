5b247afcc43e0d58106d8ac62438756529e85dc7
ishuks9 <ishuks9@gmail.com>
2021-07-20 15:55:04 +0530

    updated the uc2

--- StackImp.java
+++ StackImp.java
@@ -2,17 +2,17 @@ public class StackImp {
 	
 	public static void main(String[] args) {
 		
-		 Stack stack = new Stack(5);
-		 
+		 Stack stack = new Stack(3);
+		
 		 stack.push(56);
 		 stack.push(30);
 		 stack.push(70);
+		 stack.pop();
 		 
-		 
+		 System.out.println(stack.peek());
 
 		
 
 	
 	}
-
 }
