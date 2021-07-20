4dccc120c6b490ff60ef6a122bddcb4dd946489e
ishuks9 <ishuks9@gmail.com>
2021-07-20 16:11:26 +0530

    updated the uc4

--- QueueImp.java
+++ QueueImp.java
@@ -1,9 +1,11 @@
 public class QueueImp {
 	public static void main(String[] args) {
 		Queue queue = new Queue(3);
+	
 		queue.enqueue(56);
 		queue.enqueue(30);
 		queue.enqueue(70);
-		queue.enqueue(40);
+		queue.dequeue();
+		
 	}
 }
