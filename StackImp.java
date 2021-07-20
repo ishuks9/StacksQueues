84442ec151bf6a84d3f9e776d0cbd73a13fa1303
ishuks9 <ishuks9@gmail.com>
2021-07-20 15:48:17 +0530

    updated the uc1

---
+++ StackImp.java
@@ -0,0 +1,18 @@
+public class StackImp {
+	
+	public static void main(String[] args) {
+		
+		 Stack stack = new Stack(5);
+		 
+		 stack.push(56);
+		 stack.push(30);
+		 stack.push(70);
+		 
+		 
+
+		
+
+	
+	}
+
+}
